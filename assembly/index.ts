801ac9cec0c45e724197250bd376cf17b558fe42
Aaron Turner <aaron@aaronthedev.com>
2020-01-03 00:35:58 -0800

    Pointed to the lib instead

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,4 +1,4 @@
-export * from "../node_modules/as-bind/dist/asbind.ts";
+export * from "../node_modules/as-bind/lib/assembly/asbind.ts";
 
 export function addWorld(value: string): string {
   return value + " World!";
